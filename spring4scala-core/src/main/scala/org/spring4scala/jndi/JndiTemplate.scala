58a1fe32ea36adcad99e86bbb9a5ebf8ca85be2d
Orthanner <ort.gorthaur@yandex.ru>
2015-11-30 09:27:01 +0900

    Some cleanup

--- spring4scala-core/src/main/scala/org/spring4scala/jndi/JndiTemplate.scala
+++ spring4scala-core/src/main/scala/org/spring4scala/jndi/JndiTemplate.scala
@@ -7,7 +7,7 @@ import org.springframework.jndi.{ JndiTemplate => JavaTemplate }
 class JndiTemplate extends JavaTemplate {
   override def bind(name: String, `object`: Any): Unit = super.bind(name, `object`.asInstanceOf[Object])
 
-  def apply[T](callback: JndiCallbackFunction[T]): T = execute(new JndiCallbackAdapter[T](callback))
+  def apply[T](callback: JndiCallbackFunction[T]): T = execute(callback)
 }
 
 object JndiTemplate {
